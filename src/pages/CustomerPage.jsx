934d0337b36089240bd48fa3bdf964c712737b5e
İsmail Yücel Ölmez <ismailyucelolmez514@gmail.com>
2024-02-14 12:21:22 +0300

    Customers Page and Employees Page Implemented

--- src/pages/CustomerPage.jsx
+++ src/pages/CustomerPage.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+import { customersData, customersGrid } from '../../data/dummy';
+
 
 const CustomerPage = () => {
+
+  const selectionsettings = { persistSelection: true };
+  const toolbarOptions = ['Delete'];
+  const editing = { allowDeleting: true, allowEditing: true };
+
+
   return (
-    <div className=''>
-      customer
-    </div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+    <header>
+      <h1 className='text-3xl font-extrabold'>Customers</h1>
+    </header>
+    <GridComponent
+      dataSource={customersData}
+      enableHover={false}
+      allowPaging
+      pageSettings={{ pageCount: 5 }}
+      selectionSettings={selectionsettings}
+      toolbar={toolbarOptions}
+      editSettings={editing}
+      allowSorting
+    >
+      <ColumnsDirective>
+        
+        {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+      </ColumnsDirective>
+      <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
+    </GridComponent>
+  </div>
   )
 }
 
--- src/pages/PersonelPage.jsx
+++ src/pages/PersonelPage.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
+import { GridComponent, Inject, ColumnsDirective, ColumnDirective, Search, Page } from '@syncfusion/ej2-react-grids';
+import { employeesData, employeesGrid } from '../../data/dummy';
 
 const PersonelPage = () => {
+
+  const toolbarOptions = ['Search'];
+  const editing = { allowDeleting: true, allowEditing: true };
+
+
   return (
     <div className="m-2 md:m-10 mt-16 p-2 md:p-10 bg-white rounded-3xl">
+
       <header className='mb-8'>
 
         <h1 className='text-3xl font-extrabold'>Employees</h1>
 
       </header>
+
+      <GridComponent
+        dataSource={employeesData}
+        width="auto"
+        allowPaging
+        allowSorting
+        pageSettings={{ pageCount: 5 }}
+        editSettings={editing}
+        toolbar={toolbarOptions}
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, i) => <ColumnDirective key={i} {...item} />)}
+        </ColumnsDirective>
+
+        <Inject services={[Search, Page]} />
+
+      </GridComponent>
       
     </div>
   )
